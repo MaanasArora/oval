c9c6d6275ffbb1449320bbd03eefbd21c9a90e36
Maanas Arora <maanasarora23@gmail.com>
2026-02-19 22:51:55 -0500

    Change default decomposition method from UMAP to PCA in decompose_votes function

--- oval/decomposition.py
+++ oval/decomposition.py
@@ -1,13 +1,13 @@
 import numpy as np
 from sklearn.decomposition import PCA
-import umap
 
 
 def decompose_votes(
-    vote_matrix: np.ndarray, num_components: int = 2, method: str = "umap"
+    vote_matrix: np.ndarray, num_components: int = 2, method: str = "pca"
 ):
     vote_matrix_nonan = np.nan_to_num(vote_matrix, nan=0)
     if method == "umap":
+        import umap
         reducer = umap.UMAP(n_components=num_components, random_state=42)
     elif method == "pca":
         reducer = PCA(n_components=num_components)
